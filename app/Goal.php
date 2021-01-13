e542f051ea8e813df5a2e5232a5530cbc5a62b65
EC2 Default User <ec2-user@ip-172-31-29-241.ap-northeast-1.compute.internal>
2021-01-13 15:52:39 +0000

    add controller & model & view

---
+++ app/Goal.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Goal extends Model
+{
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+    
+    public function todos()
+    {
+        return $this->hasMany('App\Todo');
+    }
+
+}
