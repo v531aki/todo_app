4033ae1f282719f998e20da07ec3313b3367ec43
EC2 Default User <ec2-user@ip-172-31-29-241.ap-northeast-1.compute.internal>
2021-01-14 15:58:47 +0000

    set routing

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,20 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
+
+
+Route::get('/', 'HomeController@index');
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::resource("goals", "GoalController");
+
+Route::resource("goals.todos", "TodoController");
+
+Route::post('/goals/{goal}/todos/{todo}/sort', 'TodoController@sort');
+
+Auth::routes();
+
 */
 
 Route::get('/', function () {
